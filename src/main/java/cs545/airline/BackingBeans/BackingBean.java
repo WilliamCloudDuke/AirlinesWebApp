6167fb4cc9f3352b3820ed50756842c2210de0be
bertukan <bertukan2017@gmail.com>
2018-06-18 22:24:37 -0500

    Airlines Update

--- src/main/java/cs545/airline/BackingBeans/BackingBean.java
+++ src/main/java/cs545/airline/BackingBeans/BackingBean.java
@@ -3,12 +3,15 @@ package cs545.airline.BackingBeans;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import cs545.airline.model.Airline;
+import cs545.airline.model.Airport;
 import cs545.airline.service.AirlineService;
 
 @Named("backingBean")
@@ -44,6 +47,21 @@ public class BackingBean implements Serializable {
 	public List<Airline> getAirlines() {
 		airlines = airlineService.findAll();
 		return airlines;
+	}
+	
+	public String editAirLine(long id) {
+		Airline air = airlineService.findById(id);
+		Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+		System.out.println("sessionMap " + sessionMap);
+		sessionMap.put("editAirline", air);
+		return "editAirline.xhtml?faces-redirect=true";
+	}
+	
+	public String updateAirline(Airline airline) {
+		
+		airlineService.update(airline);
+		return "ListAirLines.xhtml?faces-redirect=true";
+		
 	}
 
 }
